210da195e64c3c38c32e7fdea86c14c0f6e2aaca
ZhuoyangM <zhmeng@ucsd.edu>
2022-01-19 16:41:31 -0800

    Second fix

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,11 +12,12 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            if(nextOpenBracket <0) {break;}
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            
+            if(nextOpenBracket <0 || nextCloseBracket<0 || openParen<0 || closeParen<0 ) {
+                break;
+            }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
